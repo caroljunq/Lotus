9a064ed5ee49cc1781296c8bcbf6654dc53a0b9c
Carolina Junqueiraca <junqueiracarolina@live.com>
2017-06-24 18:07:15 -0300

    div to jpg

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
 //adiciona biblioteca jquery do node_modules
 window.$ = window.jQuery = require('jquery');
 
-var teste = $('.teste').attr('class');
+var domtoimage = require('dom-to-image');
 
-console.log(teste);
+
+var node = document.getElementById('teste1');
+
+domtoimage.toJpeg(document.getElementById('teste1'), { width: 500, height: 500 })
+    .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'my-image-name.jpeg';
+        link.href = dataUrl;
+        link.click();
+    });
