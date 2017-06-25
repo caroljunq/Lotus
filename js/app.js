5f8db0ef1539aea22bd90d99dc342ddd3a677931
Carolina Junqueiraca <junqueiracarolina@live.com>
2017-06-25 01:23:04 -0300

    base app

--- js/app.js
+++ js/app.js
@@ -1,16 +1,51 @@
 
 //adiciona biblioteca jquery do node_modules
 window.$ = window.jQuery = require('jquery');
+require('bootstrap');
 
-var domtoimage = require('dom-to-image');
+const btn = $("#teste1");
+/**
+ *
+ */
+function setEvents(){
+    btn.on('click',e => digaOi());
+}
+
+function digaOi(){
+    let x = 5;
+    x=6;
+    console.log(x);
+}
+/**
+ * Init
+ */
+var init = function(){
+    setEvents();
+
+}
 
 
-var node = document.getElementById('teste1');
+init();
 
-domtoimage.toJpeg(document.getElementById('teste1'), { width: 500, height: 500 })
-    .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'my-image-name.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
+
+/**
+* This is a description
+* @namespace My.Namespace
+* @method myMethodName
+* @param {String} str - some string
+* @param {Object} obj - some object
+* @param {requestCallback} callback - The callback that handles the response.
+* @return {bool} some bool
+*/
+
+// var domtoimage = require('dom-to-image');
+
+// var node = document.getElementById('teste1');
+//
+// domtoimage.toJpeg(document.getElementById('teste1'), { width: 500, height: 500 })
+//     .then(function (dataUrl) {
+//         var link = document.createElement('a');
+//         link.download = 'my-image-name.jpeg';
+//         link.href = dataUrl;
+//         link.click();
+//     });
--- main.js
+++ main.js
@@ -9,11 +9,13 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1366, height: 768,'minHeight': 600,
+  'minWidth': 1024})
+  //mainWindow.setMenu(null);
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
